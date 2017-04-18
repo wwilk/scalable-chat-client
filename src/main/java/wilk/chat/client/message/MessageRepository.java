a28aa857f3bf42ed160e81547ed3ed34da059cc2
Wojtek Wilk <wilk.wojtek@gmail.com>
2017-04-18 22:44:18 +0200

    easier way of getting all messages

--- src/main/java/wilk/chat/client/message/MessageRepository.java
+++ src/main/java/wilk/chat/client/message/MessageRepository.java
@@ -5,10 +5,6 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
 import java.util.List;
 
 /**
@@ -25,11 +21,8 @@ public class MessageRepository {
     }
 
     public List<Message> findAll() {
-        CriteriaBuilder cb = em.getCriteriaBuilder();
-        CriteriaQuery<Message> cq = cb.createQuery(Message.class);
-        Root<Message> rootEntry = cq.from(Message.class);
-        CriteriaQuery<Message> all = cq.select(rootEntry);
-        TypedQuery<Message> allQuery = em.createQuery(all);
-        return allQuery.getResultList();
+    	return em
+        .createNativeQuery("SELECT * FROM MESSAGE", Message.class)
+        .getResultList();
     }
 }
